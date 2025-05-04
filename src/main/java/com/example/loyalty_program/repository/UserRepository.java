02b250cde8d0b68a2cd5932259615951a740254b
Hubert.Karw <hubert_karwowski@o2.pl>
2025-05-04 14:17:16 +0200

    loyalty-program/entities/creation of entities

---
+++ src/main/java/com/example/loyalty_program/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.loyalty_program.repository;
+
+import com.example.loyalty_program.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(Long aLong);
+}
